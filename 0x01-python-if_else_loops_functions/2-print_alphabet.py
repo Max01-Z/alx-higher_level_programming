8ac62682872cdb0a2a382a943e32271abe02780f
Max01-Z <mwendamarx@gmail.com>
2022-06-01 00:44:14 +0300

    add

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""Print the alphabet in lowercase, not followed by a new line."""
-
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for chr in "abcdefghijklmnopqrstuvwxyz":
+    print('{}'.format(chr), end="")
     
