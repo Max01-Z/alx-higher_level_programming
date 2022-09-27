5af5652965123e3f9b742aa89e5419bd7fcc0185
Max01-Z <mwendamarx@gmail.com>
2022-09-28 02:24:22 +0300

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""fetches from a url, displays its content"""
-from urllib import request
+"""script that fetches 'https://alx-intranet.hbtn.io/status' """
+if __name__ == "__main__":
+    import urllib.request
 
-
-if __name__ == '__main__':
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        if response.readable():
-            data = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(data)))
-            print("\t- content: {}".format(data))
-            print("\t- utf8 content: {}".format(data.decode("utf-8")))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
+        html = response.read()
+    print("Body response:")
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
